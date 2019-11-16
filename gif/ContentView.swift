bd22ac43c64e4b92fcf77359c7c31efd50c100fd
dan <dan@pourhadi.com>
2019-11-16 13:38:15 -0600

    Initial Commit

---
+++ gif/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gif
+//
+//  Created by dan on 11/16/19.
+//  Copyright © 2019 dan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
